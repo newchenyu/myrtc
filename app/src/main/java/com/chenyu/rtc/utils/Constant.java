faac7d8256b539477654ea8b9112bde33ff0c97a
chenyu <1234%^&*q>
2019-07-23 17:46:21 +0800

    七牛的一些key

--- app/src/main/java/com/chenyu/rtc/utils/Constant.java
+++ app/src/main/java/com/chenyu/rtc/utils/Constant.java
@@ -2,20 +2,19 @@ package com.chenyu.rtc.utils;
 
 public class Constant {
     // 调用服务的access key
-    public static final String QN_AK = "JPiqCIhx8zkgzrvdO3dWZhA1iIsb7l81b1yCb3HZ";
+    public static final String QN_AK = "七牛的access key";
     // 调用服务的secret key
-    public static final String QN_SK = "smSQcYSa7QBrwTzCZv9PJKWs6T-i3Or-yNKQQhxc";
+    public static final String QN_SK = "七牛的secret key";
     // 直播空间名 一般不会变
     public static final String HUB = "chenyutest";
     // 多人会议应用id 一般不会变
-    public static final String APP_ID = "e9kkfmvrb";
+    public static final String APP_ID = "应用id";
     // 直播应用id 一般不会变
-    public static final String APP_ID_LIVE = "e9kkp7q0n";
     public static final String ADMIN_USER = "admin";
     public static final String EXTRA_ROOM_TOKEN = "ROOM_TOKEN";
     public static final String EXTRA_USER_ID = "USER_ID";
     public static final String EXTRA_ROOM_ID = "ROOM_ID";
     public static final String TAG_CAMERA = "camera";
     public static final String TAG_SCREEN = "screen";
-    public static final String BUGLY_APPID = "da57e9ee15";
+    public static final String BUGLY_APPID = "腾讯bugly appid";
 }
